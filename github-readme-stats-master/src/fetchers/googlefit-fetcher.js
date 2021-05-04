20cefce72f336866d6f67e1866cf8dc57ace16dc
신다정 <ssdj0607@naver.com>
2021-05-04 14:49:16 +0900

    [S04P31A302-183] feat/dajeong/gfit/fetcher : Fix fetchGoogleFitGetData

--- github-readme-stats-master/src/fetchers/googlefit-fetcher.js
+++ github-readme-stats-master/src/fetchers/googlefit-fetcher.js
@@ -5,13 +5,7 @@ const urlParse = require("url-parse");
 const queryParse = require("query-string");
 const bodyParser = require("body-parser");
 const request = require("request");
-const weekAgo = new Date();
-weekAgo.setHours(0,0,0,0);
-const dayOfMonth = weekAgo.getDate();
-weekAgo.setDate(dayOfMonth - 6);
 
-const END = Date.parse(new Date());
-const START = Date.parse(weekAgo);
 
 const fetchGoogleFitGetUrl = async () => {
  
@@ -51,6 +45,29 @@ const fetchGoogleFitGetUrl = async () => {
 
 
 const fetchGoogleFitGetData = async (code) => {
+    const DAY = 7;
+    const weekAgo = new Date();
+    weekAgo.setHours(0,0,0,0);
+    const dayOfMonth = weekAgo.getDate();
+    weekAgo.setDate(dayOfMonth - (DAY - 1));
+
+    const END = Date.parse(new Date());
+    const START = Date.parse(weekAgo);
+
+
+
+    // 일주일 간 헬스 데이터 총합
+    const stats = {
+        step: 0,
+        distance: 0,
+        heart_rate_avg: 0,
+        heart_rate_max: 0,
+        heart_rate_min: 0,
+        active_minutes: 0,
+        heart_level: 0,
+        heart_minutes: 0,
+        sleep: 0
+    };
     
     // const queryURL = new urlParse(url);
     // console.log("---->"+url);
@@ -98,6 +115,10 @@ const fetchGoogleFitGetData = async (code) => {
                         dataTypeName: "com.google.active_minutes",
                         dataSourceId: "derived:com.google.active_minutes:com.google.android.gms:merge_active_minutes"
                     },
+                    {
+                        dataTypeName: "com.google.heart_minutes",
+                        dataSourceId: "derived:com.google.heart_minutes:com.google.android.gms:merge_heart_minutes"
+                    },
 
                 ],
                 // 86400000 밀리초 = 24시간
@@ -108,13 +129,33 @@ const fetchGoogleFitGetData = async (code) => {
 
         });
         fitnessArray = result.data.bucket
+        // console.log(fitnessArray)
         for(const dataSet of fitnessArray){
             for(const points of dataSet.dataset){
                 for(const p of points.point){
-                    console.log(p.value)
+                    if(p.dataTypeName === 'com.google.step_count.delta'){
+                        stats.step += p.value[0].intVal
+                    }
+                    if(p.dataTypeName === 'com.google.distance.delta') {
+                        stats.distance += p.value[0].fpVal
+                    }
+                    if(p.dataTypeName === 'com.google.heart_rate.summary') {
+                        stats.heart_rate_avg += p.value[0].fpVal
+                        stats.heart_rate_max += p.value[1].fpVal
+                        stats.heart_rate_min += p.value[2].fpVal
+                    }
+                    if(p.dataTypeName === 'com.google.active_minutes') {
+                        stats.active_minutes += p.value[0].intVal
+                    }
+                    if(p.dataTypeName === 'com.google.heart_minutes.summary') {
+                        stats.heart_level += p.value[0].fpVal
+                        stats.heart_minutes += p.value[1].intVal
+                    }
                 }
+           
             }
         }
+        
     } catch (e) {
         console.log(e)
     }
@@ -140,15 +181,14 @@ const fetchGoogleFitGetData = async (code) => {
         });   
         sleepArray = result.data.bucket
 
-        let sleepTime = [0, 0, 0, 0, 0, 0, 0];
         for(const dataSet of sleepArray){
             for(const points of dataSet.dataset){
                 for(const p of points.point){
                     if(p.value[0].intVal != 1){
-                        for (let i= 0;  i< 7; i++) {
+                        for (let i= 0;  i< DAY; i++) {
                             // 86400000 밀리초 = 24시간
                             if(p.startTimeNanos / 1000000 >= START + (86400000 * i)  && p.endTimeNanos / 1000000 <= START + (86400000 * (i + 1))) {
-                                sleepTime[i] += (p.endTimeNanos - p.startTimeNanos) / 1000000000 / 60
+                                stats.sleep += (p.endTimeNanos - p.startTimeNanos) / 1000000000 / 60
                             }
                             
                         }
@@ -156,12 +196,10 @@ const fetchGoogleFitGetData = async (code) => {
                 }
             }
         }
-        return sleepTime;
-        for(let i = 0; i<7; i++)
-            console.log(sleepTime[i])
     } catch (e) {
         console.log(e);
     }
+    return stats;
 };
 
 
