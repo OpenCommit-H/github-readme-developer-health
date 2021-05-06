ca061738ea271f50a4930b1cf24de1623016b960
조성훈 <sung95419@gmail.com>
2021-05-06 15:54:15 +0900

    feat/csh/chartView/chartTheme: Add themes for examples

--- github-readme-stats-master/themes/chartTheme.js
+++ github-readme-stats-master/themes/chartTheme.js
@@ -11,6 +11,30 @@ const chartThemes = {
         waka: "#f1bbba",
         fit: "#f8ecc9",
     },
+    "forest": {
+        gradientStart: "#5a9367",
+        gradientEnd: "#44633f",
+        circleLine: "maroon",
+        barLine: "gray",
+        text: "#49010f",
+        polyline: "#534847",
+        chartLine: "gray",
+        sleep: "#9b8281",
+        waka: "#d09e88",
+        fit: "#fadad8",
+    },
+    "blue": {
+        gradientStart: "#F8F9FC",
+        gradientEnd: "#A5BECF",
+        circleLine: "#020715",
+        barLine: "gray",
+        text: "#335495",
+        polyline: "#545051",
+        chartLine: "gray",
+        sleep: "#5B67A2",
+        waka: "#7B86AA",
+        fit: "#BFC8EA",
+    }
 };
 
 module.exports = chartThemes;
