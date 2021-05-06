fcd4ac1285d9aed7146bb471cf67675ec263e997
조성훈 <sung95419@gmail.com>
2021-05-06 15:33:00 +0900

    [S04P31A302-201] feat/csh/chartView/chart-card: Create chart card without themes

---
+++ github-readme-stats-master/src/cards/chart-card.js
@@ -0,0 +1,153 @@
+const chartThemes = require("../../themes/chartTheme");
+
+const renderChartCard = (data, username, themes) => {
+    var svgSizeWidth = 200;
+    var svgSizeHeight = 200;
+
+    
+    var chartStartX = 15;
+    var chartStartY = 40;
+    var chartEndX = 70;
+    var chartEndY = 40;
+    
+    var standardTime = 24;
+    
+    var dx = new Array();
+    dx.push(0);
+    dx.push(10);
+    dx.push(20);
+    dx.push(30);
+    dx.push(40);
+    dx.push(50);
+    dx.push(60);
+    
+    var maxCommit = 0;
+    data.forEach((element) => {
+        maxCommit = Math.max(maxCommit, parseInt(element.commits));
+    })
+    maxCommit += 1;
+
+    const theme = themes? chartThemes[themes] : chartThemes["defaultTheme"];
+
+    var style = `
+        <defs>
+            <linearGradient id="myGradient" gradientTransform="rotate(90)">
+                <stop offset="5%"  stop-color=${theme.gradientStart} />
+                <stop offset="95%" stop-color=${theme.gradientEnd} />
+            </linearGradient>
+        </defs>
+        <style>
+        circle {
+            fill: url('#myGradient');
+            stroke: ${theme.circleLine};
+            stroke-width: 0.4px;
+        }
+        .bar {
+            stroke: ${theme.barLine};
+            stroke-width: 0.3px;
+        }
+        text {
+            fill: ${theme.text};
+        }
+        #title {
+            text-anchor: middle;
+            font: bold 25px sans-selif;
+        }
+        #day {
+            text-anchor: middle;
+        }
+        polyline {
+            fill:none;
+            stroke:${theme.polyline};
+            stroke-width:4;
+        }
+        #chart {
+            stroke:${theme.chartLine};
+            stroke-width:0.4px;
+        }
+        #sleep {
+            fill: ${theme.sleep};
+        }
+        #waka {
+            fill: ${theme.waka};
+        }
+        #fit {
+            fill: ${theme.fit};
+        }
+        </style>
+    `;
+    
+    var svg = `
+      <svg version="1.1" width="${svgSizeWidth}" height="${svgSizeHeight}" xmlns="http://www.w3.org/2000/svg>
+        <svg width="100%" height="100%">
+        ${style}
+          <circle cx="50%" cy="50%" r="48%"/>
+          <rect id="chart" fill="none" x="${chartStartX}%" y="${chartStartY}%" width="${chartEndX}%" height="${chartEndY}%"/>
+    
+          <g id="sleep">
+            <rect class="bar" x="${chartStartX + dx[0] + 1}%" y="${chartStartY + chartEndY - (data[0].sleep / standardTime) * 40}%" width="8%" height="${(data[0].sleep / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[1] + 1}%" y="${chartStartY + chartEndY - (data[1].sleep / standardTime) * 40}%" width="8%" height="${(data[1].sleep / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[2] + 1}%" y="${chartStartY + chartEndY - (data[2].sleep / standardTime) * 40}%" width="8%" height="${(data[2].sleep / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[3] + 1}%" y="${chartStartY + chartEndY - (data[3].sleep / standardTime) * 40}%" width="8%" height="${(data[3].sleep / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[4] + 1}%" y="${chartStartY + chartEndY - (data[4].sleep / standardTime) * 40}%" width="8%" height="${(data[4].sleep / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[5] + 1}%" y="${chartStartY + chartEndY - (data[5].sleep / standardTime) * 40}%" width="8%" height="${(data[5].sleep / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[6] + 1}%" y="${chartStartY + chartEndY - (data[6].sleep / standardTime) * 40}%" width="8%" height="${(data[6].sleep / standardTime) * 40}%"/>
+          </g>
+          <g id="waka">
+            <rect class="bar" x="${chartStartX + dx[0] + 1}%" y="${chartStartY + chartEndY - (data[0].sleep / standardTime) * 40 - (data[0].waka / standardTime) * 40}%" width="8%" height="${(data[0].waka / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[1] + 1}%" y="${chartStartY + chartEndY - (data[1].sleep / standardTime) * 40 - (data[1].waka / standardTime) * 40}%" width="8%" height="${(data[1].waka / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[2] + 1}%" y="${chartStartY + chartEndY - (data[2].sleep / standardTime) * 40 - (data[2].waka / standardTime) * 40}%" width="8%" height="${(data[2].waka / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[3] + 1}%" y="${chartStartY + chartEndY - (data[3].sleep / standardTime) * 40 - (data[3].waka / standardTime) * 40}%" width="8%" height="${(data[3].waka / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[4] + 1}%" y="${chartStartY + chartEndY - (data[4].sleep / standardTime) * 40 - (data[4].waka / standardTime) * 40}%" width="8%" height="${(data[4].waka / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[5] + 1}%" y="${chartStartY + chartEndY - (data[5].sleep / standardTime) * 40 - (data[5].waka / standardTime) * 40}%" width="8%" height="${(data[5].waka / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[6] + 1}%" y="${chartStartY + chartEndY - (data[6].sleep / standardTime) * 40 - (data[6].waka / standardTime) * 40}%" width="8%" height="${(data[6].waka / standardTime) * 40}%"/>
+          </g>
+    
+          <g id="fit">
+            <rect class="bar" x="${chartStartX + dx[0] + 1}%" y="${chartStartY + chartEndY - (data[0].sleep / standardTime) * 40 - (data[0].waka / standardTime) * 40 - (data[0].fit / standardTime) * 40}%" width="8%" height="${(data[0].fit / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[1] + 1}%" y="${chartStartY + chartEndY - (data[1].sleep / standardTime) * 40 - (data[1].waka / standardTime) * 40 - (data[1].fit / standardTime) * 40}%" width="8%" height="${(data[1].fit / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[2] + 1}%" y="${chartStartY + chartEndY - (data[2].sleep / standardTime) * 40 - (data[2].waka / standardTime) * 40 - (data[2].fit / standardTime) * 40}%" width="8%" height="${(data[2].fit / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[3] + 1}%" y="${chartStartY + chartEndY - (data[3].sleep / standardTime) * 40 - (data[3].waka / standardTime) * 40 - (data[3].fit / standardTime) * 40}%" width="8%" height="${(data[3].fit / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[4] + 1}%" y="${chartStartY + chartEndY - (data[4].sleep / standardTime) * 40 - (data[4].waka / standardTime) * 40 - (data[4].fit / standardTime) * 40}%" width="8%" height="${(data[4].fit / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[5] + 1}%" y="${chartStartY + chartEndY - (data[5].sleep / standardTime) * 40 - (data[5].waka / standardTime) * 40 - (data[5].fit / standardTime) * 40}%" width="8%" height="${(data[5].fit / standardTime) * 40}%"/>
+            <rect class="bar" x="${chartStartX + dx[6] + 1}%" y="${chartStartY + chartEndY - (data[6].sleep / standardTime) * 40 - (data[6].waka / standardTime) * 40 - (data[6].fit / standardTime) * 40}%" width="8%" height="${(data[6].fit / standardTime) * 40}%"/>
+          </g>
+    
+          <svg width="100%" height="100%" viewBox="0 0 500 500" preserveAspectRatio="none">
+            <g id="infoText">
+              <g id="day">
+                <text x="${chartStartX + dx[0] + 5}%" y="${chartStartY + chartEndY + 4}%">${data[0].date}</text>
+                <text x="${chartStartX + dx[1] + 5}%" y="${chartStartY + chartEndY + 4}%">${data[1].date}</text>
+                <text x="${chartStartX + dx[2] + 5}%" y="${chartStartY + chartEndY + 4}%">${data[2].date}</text>
+                <text x="${chartStartX + dx[3] + 5}%" y="${chartStartY + chartEndY + 4}%">${data[3].date}</text>
+                <text x="${chartStartX + dx[4] + 5}%" y="${chartStartY + chartEndY + 4}%">${data[4].date}</text>
+                <text x="${chartStartX + dx[5] + 5}%" y="${chartStartY + chartEndY + 4}%">${data[5].date}</text>
+                <text x="${chartStartX + dx[6] + 5}%" y="${chartStartY + chartEndY + 4}%">${data[6].date}</text>
+              </g>
+              <text x="${chartStartX - 6}%" y="${chartStartY -2}%">Hour</text>
+              <text x="${chartStartX - 5}%" y="${chartStartY +4}%">${standardTime}</text>
+              <text x="${chartStartX - 2}%" y="${chartStartY + chartEndY - 2}%">0</text>
+
+              <text x="${chartStartX + chartEndX - 4}%" y="${chartStartY -2}%">Commit</text>
+              <text x="${chartStartX + chartEndX + 1}%" y="${chartStartY +4}%">${maxCommit}</text>
+              <text x="${chartStartX + chartEndX + 1}%" y="${chartStartY + chartEndY - 2}%">0</text>
+            </g>
+            <polyline points="${500 * (chartStartX + dx[0] + 5)/100},${500 * (chartStartY + chartEndY)/100 - (data[0].commits/maxCommit) * 200} 
+                              ${500 * (chartStartX + dx[1] + 5)/100},${500 * (chartStartY + chartEndY)/100 - (data[1].commits/maxCommit) * 200} 
+                              ${500 * (chartStartX + dx[2] + 5)/100},${500 * (chartStartY + chartEndY)/100 - (data[2].commits/maxCommit) * 200} 
+                              ${500 * (chartStartX + dx[3] + 5)/100},${500 * (chartStartY + chartEndY)/100 - (data[3].commits/maxCommit) * 200} 
+                              ${500 * (chartStartX + dx[4] + 5)/100},${500 * (chartStartY + chartEndY)/100 - (data[4].commits/maxCommit) * 200} 
+                              ${500 * (chartStartX + dx[5] + 5)/100},${500 * (chartStartY + chartEndY)/100 - (data[5].commits/maxCommit) * 200} 
+                              ${500 * (chartStartX + dx[6] + 5)/100},${500 * (chartStartY + chartEndY)/100 - (data[6].commits/maxCommit) * 200}"/>
+    
+    
+            <text id="title" x="50%" y="25%">${username}'s health stats</text>
+          </svg>
+        </svg>
+      </svg>
+    `;
+
+    return svg;
+};
+
+module.exports = renderChartCard;
