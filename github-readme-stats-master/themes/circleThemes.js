88e6ef5e597488693dfce5979b597642ce2d8f01
신다정 <ssdj0607@naver.com>
2021-05-10 23:35:17 +0900

    Merge

---
+++ github-readme-stats-master/themes/circleThemes.js
@@ -0,0 +1,10 @@
+const circleThemes = {
+    default: {
+        startColor: "#96fbc4",
+        startPer: "0%",
+        stopColor: "#f9f586",
+        stopPer: "100%",
+        textColor: "#5D5D5D",
+    },
+}
+module.exports = circleThemes;
