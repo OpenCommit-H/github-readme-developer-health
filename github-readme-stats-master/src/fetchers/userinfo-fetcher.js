2bce8a3aff8c0507cc5a143e8463dd055d529ae0
조성훈 <sung95419@gmail.com>
2021-05-13 16:45:41 +0900

    feat/csh/userInfo: make userinfo fetcher

---
+++ github-readme-stats-master/src/fetchers/userinfo-fetcher.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+
+const userinfoStats = async ({ username }) => {
+    try {
+        const getUserData = await axios.post(`http://localhost:1234/getUserInfo/${username}`);
+        const userData = {
+            username: getUserData.data.github_id,
+            wakaname: getUserData.data.waka_id,
+            api_key: getUserData.data.api_key,
+            refresh_token: getUserData.data.refresh_token
+        }
+        
+        return userData;
+    } catch (err) {
+        if (err.response.status < 200 || err.response.status > 299) {
+            throw new Error(
+              "Wakatime user not found, make sure you have a wakatime profile",
+            );
+        }
+        throw err;
+    }
+};
+
+module.exports = {
+    userinfoStats,
+};
