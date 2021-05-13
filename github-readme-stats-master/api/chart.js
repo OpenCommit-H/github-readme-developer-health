fca17e90942ca657db8c33e5ef46560482154a2a
조성훈 <sung95419@naver.com>
2021-05-13 07:50:23 +0000

    Revert "Merge branch 'csh/userInfo' into 'develop'"
    This reverts merge request !35

--- github-readme-stats-master/api/chart.js
+++ github-readme-stats-master/api/chart.js
@@ -7,15 +7,17 @@ const {
     isLocaleAvailable,
 } = require("../src/common/utils");
 const { fetchWakatimeStats } = require("../src/fetchers/wakatime-fetcher");
-const { userinfoStats } = require("../src/fetchers/userinfo-fetcher");
 const renderChartCard = require("../src/cards/chart-card");
 
 module.exports = async (req, res) => {
     const {
-      username,
-      range,
-      api_domain,
-      themes,
+        wakaname,
+        api_key,
+        range,
+        api_domain,
+        count_private,
+        include_all_commits,
+        themes,
     } = req.query;
 
     // default data
@@ -66,12 +68,8 @@ module.exports = async (req, res) => {
     ];
 
     try {
-      const userStats = await userinfoStats({ username });
-
-      const { wakaname, api_key, refresh_token } = userStats;
-      
-      
-      const wakaStats = await fetchWakatimeStats({ wakaname, api_domain, range, api_key });
+        const wakaStats = await fetchWakatimeStats({ wakaname, api_domain, range, api_key });
+        
         // find day index
         var dayIdx = 0;
         data.forEach((element, idx) => {
