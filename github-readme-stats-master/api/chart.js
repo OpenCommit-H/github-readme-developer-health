099d66d6515d6a1ca11bfe37ced5ada2d8dbb889
clalsw <lsw2524@gmail.com>
2021-05-14 11:34:17 +0900

    [S04P31A302-265] feat/lsw/chartCard/fetcher: Update commit Fetcher

--- github-readme-stats-master/api/chart.js
+++ github-readme-stats-master/api/chart.js
@@ -7,7 +7,9 @@ const {
     isLocaleAvailable,
 } = require("../src/common/utils");
 const { fetchWakatimeStats } = require("../src/fetchers/wakatime-fetcher");
+const fetchStats = require("../src/fetchers/test-fetcher");
 const renderChartCard = require("../src/cards/chart-card");
+const { fetchGoogleFitGetData, getAccessToken } = require("../src/fetchers/googlefit-fetcher");
 
 module.exports = async (req, res) => {
     const {
@@ -18,78 +20,89 @@ module.exports = async (req, res) => {
         count_private,
         include_all_commits,
         themes,
+        username,
     } = req.query;
+   // default data
+// if create total fetcher, then fit, commits, sleep will erase
+var data = [
+  {
+    "date": "Sun",
+    "commits": "0",
+  },
+  {
+    "date": "Mon",
+    "commits": "0",
+  },
+  {
+    "date": "Tue",
+    "commits": "0",
+  },
+  {
+    "date": "Wed",
+    "commits": "0",
+  },
+  {
+    "date": "Thu",
+    "commits": "0",
+  },
+  {
+    "date": "Fri",
+    "commits": "0",
+  },
+  {
+    "date": "Sat",
+    "commits": "0",
+  },
+];
 
-    // default data
-    // if create total fetcher, then fit, commits, sleep will erase
-    var data = [
-        {
-          "date": "Sun",
-          "fit": "2",
-          "commits": "1",
-          "sleep": "6",
-        },
-        {
-          "date": "Mon",
-          "fit": "3",
-          "commits": "5",
-          "sleep": "7",
-        },
-        {
-          "date": "Tue",
-          "fit": "2",
-          "commits": "12",
-          "sleep": "6",
-        },
-        {
-          "date": "Wed",
-          "fit": "0",
-          "commits": "9",
-          "sleep": "8",
-        },
-        {
-          "date": "Thu",
-          "fit": "1",
-          "commits": "8",
-          "sleep": "8",
-        },
-        {
-          "date": "Fri",
-          "fit": "3",
-          "commits": "3",
-          "sleep": "4",
-        },
-        {
-          "date": "Sat",
-          "fit": "2",
-          "commits": "0",
-          "sleep": "5.5",
-        },
-    ];
+try {
+  const wakaStats = await fetchWakatimeStats({ wakaname, api_domain, range, api_key });
+  
+  // console.log(wakaStats)
+  // find day index
+  var dayIdx = 0;
+  data.forEach((element, idx) => {
+      if (element.date == wakaStats[0].date.split(" ")[0]) {
+          dayIdx = idx;
+      }
+  });
+  
+  // input last 7 days wakatime api data
+  wakaStats.forEach((element) => {
+      dayIdx %= 7;
+      data[dayIdx++].waka = element.total_seconds / 3600;
+  });
 
-    try {
-        const wakaStats = await fetchWakatimeStats({ wakaname, api_domain, range, api_key });
-        
-        // find day index
-        var dayIdx = 0;
-        data.forEach((element, idx) => {
-            if (element.date == wakaStats[0].date.split(" ")[0]) {
-                dayIdx = idx;
-            }
-        });
-        
-        // input last 7 days wakatime api data
-        wakaStats.forEach((element) => {
-            dayIdx %= 7;
-            data[dayIdx++].waka = element.total_seconds / 3600;
-        });
+  // last 7days github api
+  const githubStats = await fetchStats(username);
+  console.log(githubStats.day7commits)
+  for (i=0; i<githubStats.day7commits.length; i++){
+    data[githubStats.day7commits[i].day].commits ++
+  }
+  // last 7days goofle fit api
+  // last 7days goofle fit api
+// var refresh_token = "";
+const access_token = await getAccessToken(refresh_token);
+const test = await fetchGoogleFitGetData(access_token);
+// // console.log(test);
 
-        // last 7days github api
-        // last 7days goofle fit api
-        // make data
+test.active_minutes.forEach((element) => {
+  dayIdx %= 7;
+  data[dayIdx++].fit = element / 60;
+})
 
-        res.send(renderChartCard(data, wakaname, themes));
-    } catch (err) {
-        return res.send(renderError(err.message, err.secondaryMessage));
-    }
+//수면시간은 잔 다음날 반영되어서 하루 빼고 시작합니다.
+//ex)화요일 저녁에 자서 수요일 아침에 깨면 그 시간은 수요일에 반영되어 있음
+dayIdx--;
+dayIdx = dayIdx == -1 ? 6 : dayIdx;
+test.sleep.forEach((element) => {
+  dayIdx %= 7;
+  data[dayIdx++].sleep = element / 60;
+})
+  // make data
+  console.log(data)
+  res.send(renderChartCard(data, wakaname, themes));
+} catch (err) {
+  return res.send(renderError(err.message, err.secondaryMessage));
+}
 }
