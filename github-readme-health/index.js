c734e304e78a31e47b26c03fa385d154a3f13252
pyoki32 <pyoki32@gmail.com>
2021-05-17 18:21:59 +0900

    [S04P31A302-289] feat/pyoki/backend/ssesion : Update ssesion Option (store) file to mongoDB

--- github-readme-health/index.js
+++ github-readme-health/index.js
@@ -4,12 +4,16 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const session = require('express-session');
-const fileStore = require('session-file-store')(session);
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+
+//const fileStore = require('session-file-store')(session);
 
 var path = require('path');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cardsRouter = require('./routes/cards');
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
@@ -21,10 +25,14 @@ app.use(session({
   secret: '@#@$MYSIGN#@$#$',
   resave: false,
   saveUninitialized: true,
-  store: new fileStore()
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_DB,
+    collection: "sessions",
+    autoRemove: 'native'
+  })
 }));
 
-var mongoose = require('mongoose');
+
 // 2. testDB 세팅
 mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
 // mongoose.connect('mongodb://localhost:17017/githupHealthDB', {useNewUrlParser: true,useUnifiedTopology: true });
