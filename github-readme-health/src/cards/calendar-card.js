0fb515807157d28360ecf2c4f73bec067bb922aa
clalsw <lsw2524@gmail.com>
2021-05-18 22:11:17 +0900

    [S04P31A302-299] feat/lsw/calendar/card : UpdateColor

--- github-readme-health/src/cards/calendar-card.js
+++ github-readme-health/src/cards/calendar-card.js
@@ -25,8 +25,8 @@ const calendarCard = (abc = {}) => {
   for (i=0; i<42; i++){
     calendarSvg += `
     <g id="Rectangle ${i+1}" filter="url(#filter${41-i}_f)">
-    <rect id="commit${i+1}" x="${5+(i%7)*100}" y="${156+parseInt((i/7)%7)*100}" width="100" height="100"/>
-    <rect x="${4.5+(i%7)*100}" y="${155.5+parseInt((i/7)%7)*100}" width="101" height="101" stroke="black"/>
+    <rect id="commit${i+1}" x="${5+(i%7)*100}" y="${156+parseInt((i/7)%7)*100}" width="100" height="100" rx="30" ry="30"/>
+    <rect x="${4.5+(i%7)*100}" y="${155.5+parseInt((i/7)%7)*100}" width="101" height="101" stroke="black" rx="30" ry="30"/>
     </g>`
   }
   var filterSvg = "";
@@ -57,11 +57,11 @@ const calendarCard = (abc = {}) => {
       if(monthActive[i][1]<level1){
         commitColor += `#commit${i+1} {fill: #ffffff;}`
       }else if(level1<=monthActive[i][1]&&monthActive[i][1]<level2){
-        commitColor += `#commit${i+1} {fill: #D7FFF1;}`
+        commitColor += `#commit${i+1} {fill: #C5E99B;}`
       }else if(level2<=monthActive[i][1]&&monthActive[i][1]<level3){
-        commitColor += `#commit${i+1} {fill: #8CD790;}`
+        commitColor += `#commit${i+1} {fill: #D499B9;}`
       }else{
-        commitColor += `#commit${i+1} {fill: #77AF9C;}`
+        commitColor += `#commit${i+1} {fill: #9055A2;}`
       }
     }
   }
@@ -70,7 +70,11 @@ const calendarCard = (abc = {}) => {
   for (i=idx; i<idx+active_minutes.length; i++){
     textNode += `
     <text x="${20+(i%7)*100}" y="${180+parseInt((i/7)%7)*100}" font-size="25">${monthActive[i][0]}</text>
-    <text x="${20+(i%7)*100}" y="${200+parseInt((i/7)%7)*100}" font-size="20">${monthActive[i][1]}분</text>`
+    `
+    if (monthActive[i][1]){
+      textNode += `
+      <text x="${20+(i%7)*100}" y="${200+parseInt((i/7)%7)*100}" font-size="20">${monthActive[i][1]} Min</text>`
+    }
   }
   return `
   <svg width="${71*size}" height="${76.1*size}" viewBox="0 0 710 761" xmlns="http://www.w3.org/2000/svg">
@@ -78,20 +82,24 @@ const calendarCard = (abc = {}) => {
 <svg width="710" height="761" viewBox="0 0 710 761" fill="none" xmlns="http://www.w3.org/2000/svg">
 <style>
 ${commitColor}
+#health commit{
+}
+#month{
+}
 </style>
 ${calendarSvg}
 <g id="week">
-<text id="SAT" fill="black" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" letter-spacing="0em"><tspan x="639.566" y="135.909">SAT</tspan></text>
-<text id="FRI" fill="black" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" letter-spacing="0em"><tspan x="540.99" y="135.909">FRI</tspan></text>
-<text id="THU" fill="black" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" letter-spacing="0em"><tspan x="436.596" y="135.909">THU</tspan></text>
-<text id="WED" fill="black" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" letter-spacing="0em"><tspan x="334.179" y="135.909">WED</tspan></text>
-<text id="TUE" fill="black" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" letter-spacing="0em"><tspan x="238.011" y="135.909">TUE</tspan></text>
-<text id="MON" fill="black" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" letter-spacing="0em"><tspan x="134.082" y="135.909">MON</tspan></text>
-<text id="SUN" fill="black" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" letter-spacing="0em"><tspan x="36.8857" y="135.909">SUN</tspan></text>
+<text id="SAT" fill="brown" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" font-weigh="900" letter-spacing="0em"><tspan x="639.566" y="135.909">SAT</tspan></text>
+<text id="FRI" fill="brown" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" font-weigh="900" letter-spacing="0em"><tspan x="540.99" y="135.909">FRI</tspan></text>
+<text id="THU" fill="brown" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" font-weigh="900" letter-spacing="0em"><tspan x="436.596" y="135.909">THU</tspan></text>
+<text id="WED" fill="brown" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" font-weigh="900" letter-spacing="0em"><tspan x="334.179" y="135.909">WED</tspan></text>
+<text id="TUE" fill="brown" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" font-weigh="900" letter-spacing="0em"><tspan x="238.011" y="135.909">TUE</tspan></text>
+<text id="MON" fill="brown" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" font-weigh="900" letter-spacing="0em"><tspan x="134.082" y="135.909">MON</tspan></text>
+<text id="SUN" fill="brown" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="20" font-weigh="900" letter-spacing="0em"><tspan x="36.8857" y="135.909">SUN</tspan></text>
 </g>
 <g id="title">
-<text id="health commit" fill="black" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="48" letter-spacing="0em" alignment-baseline="middle" text-anchor="middle"><tspan x="50%" y="50.424">${username}'s Health Commit</tspan></text>
-<text id="january" fill="black" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="28" letter-spacing="0em" alignment-baseline="middle" text-anchor="middle"><tspan x="50%" y="100.424">${month[selectedMonth-1]}</tspan></text>
+<text id="health commit" fill="brown" font-weigh="900" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="30" letter-spacing="0em" alignment-baseline="middle" text-anchor="middle"><tspan x="50%" y="50.424">${username}'s Health Commit</tspan></text>
+<text id="month" fill="brown" font-weigh="900" xml:space="preserve" style="white-space: pre" font-family="Rakkas" font-size="28" letter-spacing="0em" alignment-baseline="middle" text-anchor="middle"><tspan x="50%" y="100.424">${month[selectedMonth-1]}</tspan></text>
 </g>
 <defs>
 ${filterSvg}
